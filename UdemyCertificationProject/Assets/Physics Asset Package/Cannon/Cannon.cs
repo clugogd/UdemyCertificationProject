04987512c0bb6284313818ec8f6bb2f9076ae001
clugofs <clugo@fullsail.com>
2016-12-06 16:45:42 -0500

    Added skyboxes and removed some unnecessary assets
    Add:
    Skyboxes
    Cannon
    Projectile
    Hoop
    
    Removed:
    Standard Assets
    Extra Icons

---
+++ UdemyCertificationProject/Assets/Physics Asset Package/Cannon/Cannon.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class Cannon : MonoBehaviour {
+
+    public float thrust = 500;
+    public Transform muzzle;
+    public GameObject projectile;
+    public float rateOfFire = 1.0f;
+    public AudioClip fireSFX;
+    public GameObject fireVFX;
+
+    private AudioSource audioSource;
+    private float firingTimer = 0.0f;
+    private bool bCanFire = true;
+
+    // Use this for initialization
+    void Start ()
+    {
+        firingTimer = rateOfFire;
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        firingTimer -= Time.deltaTime;
+
+        if (firingTimer <= 0.0f)
+            bCanFire = true;
+
+        if (Input.GetButton("Fire1") && bCanFire)
+        {
+            firingTimer = rateOfFire;
+            bCanFire = false;
+            GameObject newProjectile = Instantiate(projectile, muzzle.position, muzzle.rotation);
+            newProjectile.GetComponent<Rigidbody>().AddForce(muzzle.transform.forward * thrust);
+            if( fireVFX )
+                Destroy(Instantiate(fireVFX, muzzle.position, Quaternion.identity),0.5f);
+            if( fireSFX)
+                audioSource.PlayOneShot(fireSFX);
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        /*
+        var rb = other.GetComponent<Rigidbody>();
+        rb.AddForce(muzzle.transform.forward * thrust);
+        */
+    }
+
+}
---
+++ UdemyCertificationProject/Assets/Physics Asset Package/Hoop/GoalScoring.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalScoring : MonoBehaviour
+{
+    public Highlighter highlighter;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator OnTriggerEnter(Collider other)
+    {
+        print("Goal Scored! <" + other.name + ">");
+        Destroy(other.gameObject);
+
+        GetComponent<MeshRenderer>().enabled = true;
+        yield return new WaitForSeconds(1.0f);
+        GetComponent<MeshRenderer>().enabled = false;
+
+        if (highlighter)
+        {
+            highlighter.Highlight();
+        }
+    }
+}
